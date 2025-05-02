8b5cab2f1c62dcadd4cb6d3b354700d409f64c20
Karol <karolplonka00@gmail.com>
2025-05-02 18:42:06 +0200

    got rid of lsp-zero

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -1,49 +1,86 @@
-local lsp_zero = require('lsp-zero')
-local lsp_config = require('lspconfig')
--- lsp_config.intelephense.setup({})
+-- LSP Setup without lsp-zero
 
-lsp_zero.on_attach(function(client, bufnr)
-  -- see :help lsp-zero-keybindings
-  -- to learn the available actions
-  lsp_zero.default_keymaps({buffer = bufnr})
-end)
-
+-- Setup Mason for LSP server management
 require('mason').setup({})
 require('mason-lspconfig').setup({
   ensure_installed = {},
   handlers = {
     function(server_name)
-      require('lspconfig')[server_name].setup({})
+      require('lspconfig')[server_name].setup({
+        on_attach = function(client, bufnr)
+          -- Define keybindings for LSP functions
+          local opts = { noremap=true, silent=true, buffer=bufnr }
+          
+          -- Go to definition
+          vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+          -- Go to declaration
+          vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+          -- Show implementation
+          vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+          -- Show type definition
+          vim.keymap.set('n', 'gt', vim.lsp.buf.type_definition, opts)
+          -- Show references
+          vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+          -- Rename symbol
+          vim.keymap.set('n', '<F2>', vim.lsp.buf.rename, opts)
+          -- Code actions
+          vim.keymap.set('n', '<F4>', vim.lsp.buf.code_action, opts)
+          vim.keymap.set('x', '<F4>', vim.lsp.buf.code_action, opts)
+          -- Show hover information
+          vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+          -- Show signature help
+          vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+          -- Format code
+          vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format { async = true } end, opts)
+          -- Diagnostics navigation
+          vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
+          vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
+          -- Show diagnostics in hover window
+          vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
+          -- Show diagnostics in location list
+          vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
+          -- Show workspace symbols
+          vim.keymap.set('n', '<space>ws', vim.lsp.buf.workspace_symbol, opts)
+        end,
+        -- You can add additional capabilities here if needed
+        capabilities = require('cmp_nvim_lsp').default_capabilities(),
+      })
     end,
-  },
-})
-
-lsp_config.phpactor.setup({
-  root_dir = function()
-    -- return vim.fn.expand('%:p:h')
-    return '.'
-  end,
-  single_file_support = true,
-})
-
-
-local cmp = require('cmp')
-local cmp_action = lsp_zero.cmp_action()
-cmp.setup({
-  mapping = {
-    -- Navigate between completion item
-    ['<C-j>'] = cmp.mapping.select_next_item(),
-    ['<C-k>'] = cmp.mapping.select_prev_item(),
---    ['<Enter>'] = cmp_action.toggle_completion(),
-    -- Confirm item
-    ['<Enter>'] = cmp.mapping.confirm({select = true}),
-    -- Show function signature
   }
 })
 
-lsp_zero.on_attach(function(client, bufnr)
-  lsp_zero.default_keymaps({
-    buffer = bufnr,
-    preserve_mappings = false
-  })
-end)
+-- Configure phpactor specifically
+
+-- Setup nvim-cmp for autocompletion
+local cmp = require('cmp')
+
+cmp.setup({
+  mapping = cmp.mapping.preset.insert({
+    -- Navigate between completion items
+    ['<C-j>'] = cmp.mapping.select_next_item(),
+    ['<C-k>'] = cmp.mapping.select_prev_item(),
+    
+    -- Confirm item
+    ['<Enter>'] = cmp.mapping.confirm({ select = true }),
+    
+    -- Scroll documentation
+    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    
+    -- Complete
+    ['<C-space>'] = cmp.mapping.complete(),
+    
+    -- Abort
+    ['<C-e>'] = cmp.mapping.abort(),
+  }),
+  
+  -- Configure sources for autocompletion
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+    { name = 'buffer' },
+    { name = 'path' },
+  }),
+  
+  -- You can add formatting here if needed
+  -- formatting = {...}
+})
--- .config/nvim/lua/k-roll/packer.lua
+++ .config/nvim/lua/k-roll/packer.lua
@@ -1,73 +1,60 @@
 -- This file can be loaded by calling `lua require('plugins')` from your init.vim
-
 -- Only required if you have packer configured as `opt`
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
-
+  
+  -- Telescope for fuzzy finding
   use {
-	'nvim-telescope/telescope.nvim', tag = '0.1.6',
-	-- or                            , branch = '0.1.x',
-	requires = {
-        {'nvim-lua/plenary.nvim'},
-        { "nvim-telescope/telescope-live-grep-args.nvim" },
+    'nvim-telescope/telescope.nvim', tag = '0.1.6',
+    requires = {
+      {'nvim-lua/plenary.nvim'},
+      {'nvim-telescope/telescope-live-grep-args.nvim'},
     },
   }
-
+  
   -- THEMES
   use "rebelot/kanagawa.nvim"
   use 'rose-pine/neovim'
   use 'shaunsingh/nord.nvim'
   use 'AlexvZyl/nordic.nvim'
   use "EdenEast/nightfox.nvim"
-
   use {'ojroques/nvim-hardline'}
-
-  -- rose-pine
-
-  -- rose-pine
-
- --  use({
-	-- 'rose-pine/neovim',
-	-- as = 'rose-pine',
-	-- config = function()
-	-- 	vim.cmd('colorscheme rose-pine')
-	-- end
- --  })
-
+  
+  -- Treesitter for better syntax highlighting
   use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
-
+  
+  -- UndoTree
   use('mbbill/undotree')
+  
+  -- Mason for LSP server management
+  use 'williamboman/mason.nvim'
+  use 'neovim/nvim-lspconfig'
+  use 'williamboman/mason-lspconfig.nvim'
 
-  use{
-	  'VonHeikemen/lsp-zero.nvim',
-	  branch = 'v3.x',
-	  requires = {
-		  --- Uncomment the two plugins below if you want to manage the language servers from neovim
-		  {'williamboman/mason.nvim'},
-		  {'williamboman/mason-lspconfig.nvim'},
-		  
-		  {'neovim/nvim-lspconfig'},
-		  {'hrsh7th/nvim-cmp'},
-		  {'hrsh7th/cmp-nvim-lsp'},
-		  {'L3MON4D3/LuaSnip'},
-  	}
-  }
+  -- Autocompletion
+  use 'hrsh7th/nvim-cmp'
+  use 'hrsh7th/cmp-nvim-lsp'
+  use 'hrsh7th/cmp-buffer'
+  use 'hrsh7th/cmp-path'
+  
+  -- Comments
   use {
-	  'numToStr/Comment.nvim',
-	  config = function()
-		  require('Comment').setup()
-	  end
+    'numToStr/Comment.nvim',
+    config = function()
+      require('Comment').setup()
+    end
   }
-
-  use "nvim-lua/plenary.nvim" -- don't forget to add this one if you don't have it yet!
-
+  
+  -- Plenary (dependency for several plugins)
+  use "nvim-lua/plenary.nvim"
+  
+  -- Harpoon for quick file navigation
   use {
-	  "ThePrimeagen/harpoon",
-	  branch = "harpoon2",
-	  requires = { {"nvim-lua/plenary.nvim"} }
+    "ThePrimeagen/harpoon",
+    branch = "harpoon2",
+    requires = { {"nvim-lua/plenary.nvim"} }
   }
-
 end)
