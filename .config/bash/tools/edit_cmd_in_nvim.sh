c28d520a476f2ed0e3595fc03c12819f6cbba1a1
Karol Płonka <karol.plonka@assecobs.pl>
2025-06-09 10:24:40 +0200

    bash: edit cmd in nvim - now doesnt execute command after editing

--- .config/bash/tools/edit_cmd_in_nvim.sh
+++ .config/bash/tools/edit_cmd_in_nvim.sh
@@ -2,18 +2,16 @@ edit_command_in_nvim() {
     local tmpfile=$(mktemp)
     echo "$READLINE_LINE" > "$tmpfile"
     local cursor_pos=$READLINE_POINT
-    
-    nvim -c "set filetype=sh" -c "normal! $cursor_pos|" "$tmpfile"
-    
+
+    nvim -c "set filetype=sh" -c "normal! ${cursor_pos}|" "$tmpfile"
+
     if [ -f "$tmpfile" ]; then
-        READLINE_LINE=$(cat "$tmpfile")
+        READLINE_LINE=$(<"$tmpfile")
         READLINE_POINT=${#READLINE_LINE}
-        # Execute the command immediately
-        history -s "$READLINE_LINE"
-        eval "$READLINE_LINE"
     fi
-    
+
     rm "$tmpfile"
 }
 
 bind -x '"\C-e": edit_command_in_nvim'
+
