4679a780e7b3f280d2bcbc5f16034f2e36b03f7e
Karol Płonka <karol.plonka@assecobs.pl>
2025-06-09 09:52:26 +0200

    nvim alias

--- .config/bash/bash_config.sh
+++ .config/bash/bash_config.sh
@@ -27,6 +27,8 @@ if [ -f ~/.env ]; then
   source ~/.env 
 fi
 
+alias vim='nvim'
+
 # alias ghcs='gh copilot suggest --no-prompt'
 # alias ghcs='gh copilot suggest --shell-out -t shell'
 eval "$(gh copilot alias -- bash)"
