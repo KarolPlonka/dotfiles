5573940a58dacb3edbf65d260e157ec0c167851f
karolplonka <karolplonka00@gmail.com>
2026-05-26 09:36:04 +0200

    treesitter auto install

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -5,22 +5,7 @@ return {
     config = function()
       require("nvim-treesitter").setup({
         install_dir = vim.fn.stdpath("data") .. "/site",
-      })
-      require("nvim-treesitter").install({
-        "python", "lua", "vim", "vimdoc", "query",
-        "typescript", "javascript", "json", "html", "css", "scss", "php",
-        "markdown", "markdown_inline", "yaml",
-        "terraform", "hcl",
-      })
-      vim.api.nvim_create_autocmd("FileType", {
-        pattern = {
-          "python", "lua", "javascript", "typescript",
-          "html", "css", "scss", "php", "json", "yaml",
-          "terraform", "hcl",
-        },
-        callback = function()
-          vim.treesitter.start()
-        end,
+        auto_install = true,
       })
     end,
   },
